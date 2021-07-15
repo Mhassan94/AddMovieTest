883b401a797026b73b7128b2034906f966652181
Mohsin hassan <mhassan94@live.com>
2021-07-15 13:54:52 -0400

    add more tests

--- Tests/src/tests/testAddMoview.java
+++ Tests/src/tests/testAddMoview.java
@@ -25,12 +25,16 @@ public class ApplyAsDeveloperTest {
        login();
    }
 
+   // SANITY TESTS
+
    @Test
    public void verifyAllElementDisplay() {
        Assert.assertTrue(title.isDisplayed());
        Assert.assertTrue(releaseDate.isDisplayed());
        Assert.assertTrue(title.ratings());
    }
+
+   //POSITIVE TESTS
 
    @Test
    public void verifyAddingMovie() {
@@ -39,6 +43,39 @@ public class ApplyAsDeveloperTest {
        Assert.assertEqual("Successfully Added Movie", msg);
    }
 
+   @Test
+   public void missingField(){
+       title.sendKeys("Nightmare on Elmstreet");
+       //Missing release Date
+       rating.sendKeys("4");
+       addMovieBtn.click()
+       msg = getMessage();
+       Assert.assertEqual("All Fields Must Be Field In", msg);
+   }
+
+   // NEGATIVE TESTS
+
+   @Test
+   public void titleWithMoreThanMaxChars() {
+       title.sendKeys(""); //Send more than 200 keys
+       addMovieBtn.clcik();
+       msg = getMessage();
+       Assert.assertEqual("Cannot Accept more than 200 Characters", msg);
+   }
+
+   public void dateBelowRequiredDate() {
+       releaseDate.sendKeys("1/10/2014"); //one year less than required date
+       addMovieBtn.clcik();
+       msg = getMessage();
+       Assert.assertEqual("Date Must Be Above 1/10/2015", msg);
+   }
+
+   public void invalidRating() {
+       rating.sendKeys("7"); //Must Be between 1-5
+       addMovieBtn.clcik();
+       msg = getMessage();
+       Assert.assertEqual("Invalid Rating Number", msg);
+   }
 
 
     @After
